c4ccb1b3e806b687e3f527b0fff16c5b94a5a21d
Eli Orona <eliorona@live.com>
2021-09-14 00:42:06 -0700

    Basic site layout + start some stuff

--- server.js
+++ server.js
@@ -5,14 +5,44 @@ const pug = require('pug')
 
 app.set('view engine', 'pug')
 
-app.get('/', function (req, res) {
-   res.render('pages/index', {
-     title: 'Kiln Graphics',
-     viewPath: path.resolve(__dirname, 'views', 'pages'),
-     basedir: path.resolve(__dirname, 'views', 'partials'),
+const defaultOptions = {
+  viewPath: path.resolve(__dirname, 'views', 'pages'),
+  basedir: path.resolve(__dirname, 'views', 'partials')
+}
 
-   })
+app.get('/', function (req, res) {
+   res.render('pages/index', {...defaultOptions, ...{
+     title: 'Kiln Graphics',
+     page: "home"
+   }})
 })
+
+app.use(express.static(__dirname + "/public"))
+
+app.get("*", async function (req, res, next) {
+  try {
+    throw {err: 404}
+  } catch (err) {
+    return next(err)
+  }
+})
+
+app.use(function (err, req, res, next) {
+  res.status(err.err || 500).render('pages/error', {...defaultOptions, ...{
+    title: 'Kiln Graphics | ' + (err.err || 500),
+    error: err.err || 500,
+    page: "error"
+  }})
+})
+//
+// app.get("*", function (req, res) {
+//   res.render("pages/error", {...defaultOptions, ...{
+//     title: 'Kiln Graphics | ' + 404,
+//     error: 404,
+//     page: "error"
+//   }})
+//   res.status(404)
+// })
 
 var server = app.listen(3000, function () {
    var host = server.address().address
